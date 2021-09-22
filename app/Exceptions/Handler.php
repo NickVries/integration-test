e70962c1739df5a1561d9f6c5e1255c33ce4071a
John Zwarthoed <johnzwarthoed@sterrkdeta.nl>
2021-09-22 10:04:46 +0200

    Implement json error handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,10 +5,21 @@ declare(strict_types=1);
 namespace App\Exceptions;
 
 use App\Authentication\Domain\Exceptions\AuthRequestException;
+use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    /** @var ResponseFactory */
+    private $responseFactory;
+
+    /** @var bool */
+    private $debug;
+
     /**
      * A list of the exception types that are not reported.
      *
@@ -24,4 +35,99 @@ class Handler extends ExceptionHandler
      * @var array
      */
     protected $dontFlash = [];
+
+    public function setResponseFactory(ResponseFactory $factory): self
+    {
+        $this->responseFactory = $factory;
+
+        return $this;
+    }
+
+    public function setDebug(bool $debug): self
+    {
+        $this->debug = $debug;
+
+        return $this;
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param Request   $request
+     * @param Throwable $exception
+     * @return JsonResponse
+     */
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof ValidationException) {
+            return $this->responseFactory->json(
+                [
+                    'errors' => $this->mapValidationException($exception),
+                ],
+                $exception->getCode(),
+                [
+                    'Content-Type' => 'application/vnd.api+json',
+                ]
+            );
+        }
+
+        $error = [
+            'status' => $exception->getCode(),
+            'message' => $exception->getMessage(),
+        ];
+
+        if ($this->debug === true) {
+            $error['trace'] = $this->getTrace($exception);
+        }
+
+        return $this->responseFactory->json(
+            [
+                'errors' => [
+                    $error,
+                ],
+            ],
+            $exception->getCode(),
+            [
+                'Content-Type' => 'application/vnd.api+json',
+            ]
+        );
+    }
+
+    private function mapValidationException(ValidationException $exception): array
+    {
+        $validator = $exception->validator;
+        $invalidAttributes = array_keys($validator->failed());
+        $errors = [];
+
+        foreach ($invalidAttributes as $invalidAttribute) {
+            $errorMessages = $validator->errors()->get($invalidAttribute);
+
+            foreach ($errorMessages as $errorMessage) {
+                $pointer = str_replace('.', '/', $invalidAttribute);
+                $title = strpos($errorMessage, 'required') !== false ? 'Missing input' : 'Invalid input';
+                $errors[] = [
+                    'status' => $exception->getCode(),
+                    'message' => $exception->getMessage(),
+                    'title' => $title,
+                    'detail' => $errorMessage,
+                    'source' => ['pointer' => $pointer],
+                ];
+            }
+        }
+
+        return $errors;
+    }
+
+    private function getTrace(Throwable $exception): string
+    {
+        try {
+            if (json_encode($exception->getTrace()) === false) {
+                return 'Trace is not available.';
+            }
+        } catch (Throwable $e) {
+            return 'Trace is not available.';
+        }
+
+        return json_encode($exception->getTrace());
+    }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,8 +7,13 @@ namespace App\Providers;
 use App\Authentication\Domain\AuthorizationSession;
 use App\Authentication\Domain\AuthServer;
 use App\Authentication\Domain\AuthServerInterface;
+use App\Exceptions\Handler;
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\ServiceProvider;
+
 use function config;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +25,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        $this->app->singleton(ExceptionHandler::class, function (Container $app) {
+            return (new Handler($app))
+                ->setResponseFactory($app->make(ResponseFactory::class))
+                ->setDebug((bool) config('app.debug'));
+        });
+
         $this->app->singleton(AuthServerInterface::class, fn() => new AuthServer(
             (string) config('services.remote.oauth2.client_id'),
             (string) config('services.remote.oauth2.client_secret'),
